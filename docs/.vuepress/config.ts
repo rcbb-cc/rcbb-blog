0ddd11a6cb7ceb6f0625b85cb9dce7daf5567679
rcbb.cc <www.rcbb.cc@gmail.com>
2022-10-18 17:50:19 +0800

    feature：调整收藏的Spring文章位置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -132,7 +132,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         blogger: {
             avatar: 'https://rcbb-public.oss-cn-guangzhou.aliyuncs.com/avatar.jpg',
             name: '日常bb',
-            slogan: '积跬步以至千里~'
+            slogan: '想起来全是问题，做起来才有答案。'
         },
 
         // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
